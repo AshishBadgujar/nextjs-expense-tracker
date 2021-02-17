364befa7b8057168dd572ede809433d0072d0d33
ashish <ashishbadgujar124@gmail.com>
2021-02-17 09:47:13 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-const withTM = require("next-transpile-modules")([
+const withTM = require("next-transpile-modules")
+const withPWA = require("next-pwa")
+
+module.exports = withTM([
     "@amcharts/amcharts4/core",
     "@amcharts/amcharts4/charts",
     "@amcharts/amcharts4/themes/animated"
-    ]); // pass the modules you would like to see transpiled
-    
-    module.exports = withTM();
+]); // pass the modules you would like to see transpiled
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        swSrc: "service-worker.js"
+    }
+});
